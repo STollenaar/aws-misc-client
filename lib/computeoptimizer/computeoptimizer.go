4a1f738a8bbf98ddd3e78625e2f5c76cb1a6d618
Stephen Tollenaar <stephen.tollenaar@radient360.com>
2022-08-18 21:04:08 -0230

    expanding computeoptimizer

--- lib/computeoptimizer/computeoptimizer.go
+++ lib/computeoptimizer/computeoptimizer.go
@@ -2,7 +2,10 @@ package computeoptimizer
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"log"
+	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
@@ -31,6 +34,7 @@ func init() {
 	}
 }
 
+// CreateOptimizer, creates a recommendation preference for a resource
 func (c *ComputeOptimizerClient) CreateOptimizer(input OptimizerInput) (string, error) {
 	_, err := computeoptimizerClient.PutRecommendationPreferences(context.TODO(), &computeoptimizer.PutRecommendationPreferencesInput{
 		ResourceType:                  input.ResourceType,
@@ -42,4 +46,67 @@ func (c *ComputeOptimizerClient) CreateOptimizer(input OptimizerInput) (string, 
 	})
 
 	return input.ResourceArn, err
+}
+
+// GetRecommendationPreferences, get the basic preferences of a specific resource type
+func (c *ComputeOptimizerClient) GetRecommendationPreferences(input OptimizerInput) ([]types.RecommendationPreferencesDetail, error) {
+
+	out, err := computeoptimizerClient.GetRecommendationPreferences(context.TODO(), &computeoptimizer.GetRecommendationPreferencesInput{
+		ResourceType: input.ResourceType,
+		Scope: &types.Scope{
+			Name:  "ResourceArn",
+			Value: aws.String(input.ResourceArn),
+		},
+	})
+	return out.RecommendationPreferencesDetails, err
+}
+
+// GetEC2InstanceRecommentdations, get the recommendations of an ec2 instance
+func (c *ComputeOptimizerClient) GetEC2InstanceRecommentdations(input OptimizerInput) (types.InstanceRecommendation, error) {
+	out, err := computeoptimizerClient.GetEC2InstanceRecommendations(context.TODO(), &computeoptimizer.GetEC2InstanceRecommendationsInput{
+		InstanceArns: []string{
+			input.ResourceArn,
+		},
+	})
+
+	if err != nil {
+		return types.InstanceRecommendation{}, err
+	}
+	if len(out.Errors) > 0 {
+		return types.InstanceRecommendation{}, flattenErrors(out.Errors)
+	}
+
+	return out.InstanceRecommendations[0], nil
+}
+
+// GetAutoscalingRecommendations, get the recommendations of an autoscaling group
+func (c *ComputeOptimizerClient) GetAutoscalingRecommendations(input OptimizerInput) (types.AutoScalingGroupRecommendation, error) {
+	out, err := computeoptimizerClient.GetAutoScalingGroupRecommendations(context.TODO(), &computeoptimizer.GetAutoScalingGroupRecommendationsInput{
+		AutoScalingGroupArns: []string{
+			input.ResourceArn,
+		},
+	})
+
+	if err != nil {
+		return types.AutoScalingGroupRecommendation{}, err
+	}
+	if len(out.Errors) > 0 {
+		return types.AutoScalingGroupRecommendation{}, flattenErrors(out.Errors)
+	}
+
+	return out.AutoScalingGroupRecommendations[0], nil
+}
+
+func flattenErrors(rErrors []types.GetRecommendationError) error {
+	var messages []string
+
+	for _, e := range rErrors {
+		messages = append(messages, fmt.Sprintf(
+			"Error Code: %s, Identifier: %s, Message: %s",
+			*e.Code,
+			*e.Identifier,
+			*e.Message,
+		))
+	}
+	return errors.New(strings.Join(messages, "\n"))
 }
